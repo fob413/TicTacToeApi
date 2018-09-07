f4ad2a64be48f1a56aec6e3183752848b6a8d836
fob1493 <fob1493@gmail.com>
2018-09-07 21:46:09 +0100

    setup game controller

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask_restful import Api
 from flask_bcrypt import Bcrypt
+from app.main.controller.game import Game
 
 
 from .config import config_by_name
@@ -10,7 +12,10 @@ flask_bcrypt = Bcrypt()
 
 def create_app(config_name):
     app = Flask(__name__)
+    api = Api(app)
     app.config.from_object(config_by_name[config_name])
     flask_bcrypt.init_app(app)
+
+    api.add_resource(Game, '/')
 
     return app
---
+++ app/main/controller/game.py
@@ -0,0 +1,10 @@
+from flask import request, jsonify
+from flask_restful import Resource
+from app.main.service.tictactoe import play_game
+
+
+class Game(Resource):
+    def get(self):
+        if request.method == "GET":
+            """Play Tic Tac Toe"""
+            return play_game()
