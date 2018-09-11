dbcaf06652611f75fc19e8c62799fe7531f39326
fob1493 <fob1493@gmail.com>
2018-09-11 19:09:34 +0100

    minimax algorithm application

--- app/main/utils/game.py
+++ app/main/utils/game.py
@@ -2,6 +2,10 @@ import random
 
 from app.main.utils.helpers import response, possible_moves
 
+# set up players
+server_player = 'o'
+user_player = 'x'
+
 
 def is_game_won(board, player):
 	"""
@@ -9,10 +13,7 @@ def is_game_won(board, player):
 	returns True or False and the player that won
 	"""
 
-	is_won = {
-		'won': False,
-		'player': ''
-	}
+	is_won = False
 
 	if (
 		board[0] == board[1] == board[2] == player or
@@ -24,11 +25,8 @@ def is_game_won(board, player):
 		board[0] == board[4] == board[8] == player or
 		board[2] == board[4] == board[6] == player
 	):
-		is_won = {
-			'won': True,
-			'player': player
-		}
-	
+		is_won = True
+
 	return is_won
 
 
@@ -57,111 +55,163 @@ def random_move(board):
 def is_draw(board):
 	"""
 	Check if game is a draw
+	returns a Boolean
 	"""
-	user_player = is_game_won(board, 'x')
-	server_player = is_game_won(board, 'o')
+	if not is_game_won(board, user_player) and not is_game_won(board, server_player):
+		check = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-	if not user_player['won'] and not server_player['won']:
-		draw_game = board.find(' ')
-		if draw_game < 0:
-			return True
-		
-		else:
-			return False
+		for item in check:
+
+			if item in board:
+				return False
+
+		return True
 	
 	else:
 		return False
 
 
-def defense_win_play(possible_moves, board):
+def get_possible_moves(board):
 	"""
-	Server blocks a possible user win or
-	plays in a possible server win
-	return boolean of possible_win with the current board
+	gets all available spots on the board
+	returns a list of available spots
+	"""
+	possible_moves = []
+
+	for count, player in enumerate(board):
+		if player is not server_player and player is not user_player:
+			possible_moves.append(count)
+
+	return possible_moves
+
+
+def convert_board(board):
+	"""
+	converts the string board
+	returns a list of the board
+	with the players and the index
+	of empty spaces
 	"""
 	board_copy = list(board)
-	server_move = {
-		'played': False,
-		'board': ''.join(board_copy)
-	}
+	for count, instance in enumerate(board_copy):
+		if board_copy[count] == ' ':
+			board_copy[count] = count
+	return board_copy
 
-	# server possible win
-	for move in possible_moves:
-		if board_copy[move[0][0]] == board_copy[move[0][1]] == 'o':
-			# validate the possible move
-			if board_copy[move[1]] != ' ':
-				continue
 
-			board_copy[move[1]] = 'o'
-			server_move['played'] = True
-			server_move['board'] = ''.join(board_copy)
-			return server_move
+def minimax(board, player):
+	"""
+	implements minimax algorithm for decision theory
+	returns the best possible move for the server
+	"""
+	# store all the possible moves
+	possible_moves = get_possible_moves(board)
 
-	# block possible user win
-	for move in possible_moves:
-		if board_copy[move[0][0]] == board_copy[move[0][1]] == 'x':
-			# validate the possible move
-			if board_copy[move[1]] != ' ':
-				continue
+	# generate the score for the board
+	if is_game_won(board, server_player):
+		return 10
 
-			board_copy[move[1]] = 'o'
-			server_move['played'] = True
-			server_move['board'] = ''.join(board_copy)
-			return server_move
+	elif is_game_won(board, user_player):
+		return -10
 
-	return server_move
+	elif is_draw(convert_board(board)):
+		return 0
+
+	moves = []
+
+	for player_move in possible_moves:
+		move = {
+			'index': player_move
+		}
+
+		board[player_move] = player
+		
+		if player is server_player:
+			result = minimax(board, user_player)
+
+			if type(result) is int:
+				move['score'] = result
+
+			elif type(result) is dict:
+				move['score'] = result['score']
+
+		else:
+			result = minimax(board, server_player)
+
+			if type(result) is int:
+				move['score'] = result
+
+			elif type(result) is dict:
+				move['score'] = result['score']
+
+		board[player_move] = move['index']
+		moves.append(move)
+
+	# get and return the best move
+	if player is server_player:
+		best_score = -10000
+
+		for count, move in enumerate(moves):
+
+			if move['score'] > best_score:
+				best_score = move['score']
+				best_move = count
+
+	else:
+		best_score = 10000
+
+		for count, move in enumerate(moves):
+
+			if move['score'] < best_score:
+				best_score = move['score']
+				best_move = count
+
+	return moves[best_move]
+
+
+def play_move(board, move):
+	"""
+	server plays in the index sent
+	returns the board with the play
+	"""
+	board_copy = list(board)
+
+	board_copy[move] = 'o'
+	return ''.join(board_copy)
 
 
 def server_play(board):
 	"""
 	Server plays
 	returns the response which contains
-	the board with the servers play
+		the board with the servers play
 	"""
+	best_move = minimax(convert_board(board), server_player)['index']
+	server_move = play_move(board, best_move)
 
-	server_move = defense_win_play(possible_moves, board)
-
-	if server_move['played']:
-		# check if server has won the current game
-		if is_game_won(server_move['board'], 'o')['won']:
-			return response(
-				'Player o has won the game!!!',
-				server_move['board'],
-				200
-			)
-
-		# check if current board is a draw
-		elif is_draw(server_move['board']):
-			return response(
-				'Draw!!!',
-				server_move['board'],
-				200
-			)
-
-		else:
-			return response(
-				'Your turn',
-				server_move['board'],
-				200
-			)
-
-	else:
-		# random server play
-		random_server_play = random_move(board)
+	if server_move:
 
 		# check if server has won the current game
-		if is_game_won(random_server_play, 'o')['won']:
+		if is_game_won(server_move, server_player):
 			return response(
-				'Player ' + is_game_won(random_server_play, 'o')['player'] + ' has won the game!!!',
-				random_server_play,
+				'Player o has won the game.',
+				server_move,
 				200
 			)
 
-		# check if current board is a draw
-		elif is_draw(random_server_play):
+		# check if user player has won the current game
+		elif is_game_won(server_move, user_player):
+			return response(
+				'Player x has won the game.',
+				server_move,
+				200
+			)
+
+		# check if the current game is a draw
+		elif is_draw(convert_board(server_move)):
 			return response(
 				'Draw!!!',
-				random_server_play,
+				server_move,
 				200
 			)
 
@@ -169,6 +219,8 @@ def server_play(board):
 		else:
 			return response(
 				'Your turn',
-				random_server_play,
+				server_move,
 				200
 			)
+
+	
