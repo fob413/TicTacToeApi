43cddb827342cb86bd96cb40ded1e489f9a309bf
Oluwafunso Oluyole-Balogun <oluwafunsooluyole-balogun@Oluwafunsos-MacBook-Pro-2.local>
2023-06-30 00:49:40 -0300

    Updates requirements

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,20 +1,15 @@
 from flask import Flask
 from flask_restful import Api
-from flask_bcrypt import Bcrypt
 from app.main.controller.game import Game
 
 
 from .config import config_by_name
 
 
-flask_bcrypt = Bcrypt()
-
-
 def create_app(config_name):
     app = Flask(__name__)
     api = Api(app)
     app.config.from_object(config_by_name[config_name])
-    flask_bcrypt.init_app(app)
 
     api.add_resource(Game, '/')
 
